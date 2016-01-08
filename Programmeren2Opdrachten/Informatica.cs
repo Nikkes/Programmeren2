1d54b9d49f4465dda926e5d8a0ce47ba2ce0c164
Joris Lops <jorislops@gmail.com>
2016-01-08 11:31:23 +0100

    kleine fout in courses & VakNr opgelost

--- Programmeren2Opdrachten/Informatica.cs
+++ Programmeren2Opdrachten/Informatica.cs
@@ -35,14 +35,14 @@ namespace Programmeren2Opdrachten
             jan, piet, klaas, katrijn
         };
 
-        private static Course cSharp =  new Course() { VakNr = 1, Naam = "C#", Teacher = "Joris" };
-        private static Course math =    new Course() { VakNr = 2, Naam = "Wiskunde", Teacher = "Jos" };
-        private static Course coo =     new Course() { VakNr = 3, Naam = "Computer Oranisation", Teacher = "Sibbele" };
-        private static Course se =      new Course() { VakNr = 3, Naam = "Software Engineering", Teacher = "David" };
-        private static Course python  = new Course() { VakNr = 3, Naam = "Python", Teacher = "Wouter" };
+        private static Course cSharp =  new Course() { VakNr = 1, Name = "C#", Teacher = "Joris" };
+        private static Course math =    new Course() { VakNr = 2, Name = "Wiskunde", Teacher = "Jos" };
+        private static Course coo =     new Course() { VakNr = 3, Name = "Computer Organisation", Teacher = "Sibbele" };
+        private static Course se =      new Course() { VakNr = 4, Name = "Software Engineering", Teacher = "David" };
+        private static Course python  = new Course() { VakNr = 5, Name = "Python", Teacher = "Wouter" };
 
         private static List<Course> courses = new List<Course>() {
-            cSharp, math, coo
+            cSharp, math, coo, se, python
         };
 
         private static List<Exam> exams = new List<Exam>() {
